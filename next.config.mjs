95dc0242fb2c1d2e0c18718e014028b73a33af7d
= <=>
2024-01-30 08:43:19 +0900

    chore: nextjs cors setting

--- next.config.mjs
+++ next.config.mjs
@@ -76,20 +76,44 @@ if (mode !== "export") {
         source: "/sharegpt",
         destination: "https://sharegpt.com/api/conversations",
       },
+      // {
+      //   source: "/api/foodinko/:path*",
+      //   destination: "https://localhost:3000/:path*",
+      // },
+      // {
+      //   source: "/api/foodinko/:path*",
+      //   destination: "http://3.26.233.124/:path*",
+      // },
+      // {
+      //   source: "/api/foodinko/:path*",
+      //   destination: "http://3.26.233.124/:path*",
+      // },
+      // {
+      //   source: "/api/foodinko/:path*",
+      //   destination: "http://3.26.233.124/:path*",
+      // },
+      // {
+      //   source: "/api/user/:path*",
+      //   destination: "http://3.26.233.124/:path*",
+      // },
+      // {
+      //   source: "/api/chat/:path*",
+      //   destination: "http://3.26.233.124/:path*",
+      // },
+      // {
+      //   source: "/api/analytics/:path*",
+      //   destination: "http://3.26.233.124/:path*",
+      // },
       {
-        source: "/api/foodinko/:path*",
-        destination: "https://localhost:3000/:path*",
-      },
-      {
-        source: "/api/user/:path*",
+        source: "/user/:path*",
         destination: "http://3.26.233.124/:path*",
       },
       {
-        source: "/api/chat/:path*",
+        source: "/chat/:path*",
         destination: "http://3.26.233.124/:path*",
       },
       {
-        source: "/api/analytics/:path*",
+        source: "/analytics/:path*",
         destination: "http://3.26.233.124/:path*",
       },
     ];
